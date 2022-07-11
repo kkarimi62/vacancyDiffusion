fa413564db25a0ecd8cf8325f237d32919ae3bc6
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-07-11 14:38:14 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 				0:'NiCoCrNatom1KTemp0K', 
 				1:'NiNatom1KTemp0K', 
 				2:'NiCoCrNatom10KTemp1300K', 
-				3:'NiCoCrNatom1Kannealed', 
+				3:'NiCoCrNatom10Kannealed', 
 				4:'NiCoCrNatom1000Temp1300', 
 			   }[3]
 	sourcePath = os.getcwd() +\
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 				0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
 				6:' -var T 300 -var DataFile Equilibrated_300.dat',
 				4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 1000 -var ParseData 1 -var WriteData swapped_600.dat', 
-				5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 1000 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
+				5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 10000 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
 				51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
 				7:' -var buff 0.0 -var T 600.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_600.dat',
 				71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 1000 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
