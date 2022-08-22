4e7f054cd990ce5e6123540d484c596fb245a115
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-08-23 00:01:03 +0200

    updates

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -6,8 +6,7 @@ if __name__ == '__main__':
 	lnums = [ 37, 102   ]
 	string=open('simulations.py').readlines() #--- python script
 	#---
-	PHI = {1: 1040.0, 2: 1080.0, 3: 1120.0, 4: 1160.0, 5: 1200.0, 6: 1240.0, 7: 1280.0, 8: 1320.0, 9: 1360.0, 10: 1400.0}
-#dict(zip(range(11),np.linspace(1000,1400,11)))
+	PHI = dict(zip(range(11),np.linspace(1000,1400,11)))
 #		{
 #             '0':1,
 #             '1':2**3,
