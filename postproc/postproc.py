3f81626fbc0d2324bf09ed0a67dbbab370e2be03
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-10-23 09:27:07 +0200

    update

--- postproc/postproc.py
+++ postproc/postproc.py
@@ -21,7 +21,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 if __name__ == '__main__':
 	import os
 #
-	runs	 = [0,1,2]
+	runs	 = range(8)
 	jobname  = {
 				'3':'CantorNatom16KTemp1000K_ensemble', 
 				'4':'CantorNatom16KTemp1400K_ensemble_min', 
