82eaf6731ed43c3f6ac9d93b8ea270a6fddf684b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-11-13 10:52:22 +0100

    update

--- postproc/submitPY.py
+++ postproc/submitPY.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 	lnums = [ 27, 32 ]
 	string=open('postproc.py').readlines() #--- python script
 	#---
-	PHI  = dict(zip(range(6),np.linspace(1000,2000,6)))
-#	PHI  = dict(zip(range(11),np.arange(1000,1440,40)))
+#	PHI  = dict(zip(range(6),np.linspace(1000,2000,6)))
+	PHI  = dict(zip(range(11),np.arange(1000,1440,40)))
 #		{ 
 #            '0':'FeNi',
 #            '1':'CoNiFe',
@@ -24,10 +24,11 @@ if __name__ == '__main__':
 	for key in PHI:
 			#---	
 				inums = lnums[ 0 ] - 1
-				string[ inums ] = "\t\'4\':\'CantorNatom16KTemp%sKEnsemble8\',\n" % (int(PHI[key])) #--- change job name
+#				string[ inums ] = "\t\'4\':\'CantorNatom16KTemp%sKEnsemble8\',\n" % (int(PHI[key])) #--- change job name
+				string[ inums ] = "\t\'4\':\'NiNatom16KTemp%sK\',\n" % (int(PHI[key])) #--- change job name
 		#---	densities
 				inums = lnums[ 1 ] - 1
-				string[ inums ] = "\t\'4\':\'/../simulations/CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[key]))
+				string[ inums ] = "\t\'4\':\'/../simulations/NiNatom16KTemp%sK\',\n"%(int(PHI[key]))
 		#
 				sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
 				os.system( 'python3 junk%s.py'%count )
