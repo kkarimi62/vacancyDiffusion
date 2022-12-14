863d0af8da0ed111c4e6ac9aa4867a2b165ccd58
kkarimi62 <kamran.karimi1362@gmail.com>
2022-12-14 13:32:06 +0100

    updates

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 			#---	
 			#---	densities
 				inums = lnums[ 0 ] - 1
-				string[ inums ] = "\t3:\'NiNatom16KTemp%sK\',\n"%(int(PHI[iphi])) #--- change job name
+				string[ inums ] = "\t1:\'NiNatom16KTemp%sK\',\n"%(int(PHI[iphi])) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
 				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(PHI[iphi])
