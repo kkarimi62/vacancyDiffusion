9555d8c1df93c9badfd1cde538c14ca7f1387e6c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-12-16 21:54:30 +0100

    update

--- postproc/postproc.py
+++ postproc/postproc.py
@@ -23,14 +23,14 @@ if __name__ == '__main__':
 #
 	runs	 = range(8)
 	jobname  = {
-				'3':'CantorNatom16KTemp1000K_ensemble', 
+				'3':'NiCoCrNatom1KTemp1000K', 
 				'4':'CantorNatom16KTemp1400K_ensemble_min', 
-				}['4']
+				}['3']
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
-								'3':'/../simulations/CantorNatom16KTemp1000K_ensemble',
+								'3':'/../simulations/NiCoCrNatom1KTemp1000K',
 								'4':'/../simulations/CantorNatom16KTemp1400K_ensemble_min',
- 							}['4'] #--- source
+ 							}['3'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '23:59:59'
 	mem = '8gb'
