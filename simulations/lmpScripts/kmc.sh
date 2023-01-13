28926ab999fdf61db4b29d9e3cdc3ab1a4751cba
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-01-13 22:01:46 +0100

    updates

--- simulations/lmpScripts/kmc.sh
+++ simulations/lmpScripts/kmc.sh
@@ -26,9 +26,9 @@ setenv ATOMIC_SYMBOLS					'Ni Co Cr Fe Mn' #"Ni Co Cr" #'Ni Co Cr Fe Mn' #"Ni Co
 #setenv NTRAVAILLEUR                     3     # The number of cores associated with forces calculations per ARTnouveau search (default:1)
 ###################################### Restart options #####################################################
 
-setenv RESTART_KMC                   .false.  # IF true, restart from previous run
+setenv RESTART_KMC                   .true.  # IF true, restart from previous run
 setenv RESTART_FILE               "this_conf" # The file name used to continue a simulation from where it was last stopped
-setenv RESTART_IMPORT                .false.  # Start a NEW simulation but with the current KMC event catalogue (events.uft and topos.list)
+setenv RESTART_IMPORT                .true.  # Start a NEW simulation but with the current KMC event catalogue (events.uft and topos.list)
 setenv NEW_CATALOGUE                 .false.  #don't change! IF true, will continue simulation but will rebuild event catalogue from scratch
 
 
