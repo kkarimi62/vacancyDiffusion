de94e50f799ccabf72b9a81f968f5f5d3e78112c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-03-12 09:02:22 +0100

    updates

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -3,14 +3,13 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-#	lnums = [ 34, 104   ]
-	lnums = [ 37, 103   ]
-#	string=open('simulations-ncbj.py').readlines() #--- python script
-	string=open('simulations.py').readlines() #--- python script
+	lnums = [ 33, 96   ]
+#	lnums = [ 37, 103   ]
+	string=open('simulations-ncbj.py').readlines() #--- python script
+#	string=open('simulations.py').readlines() #--- python script
 	#---
-	PHI = dict(zip(range(6),np.linspace(1000,2000,6)))
-#	PHI = dict(zip(range(11),np.arange(300,1000,100)))
-#	PHI = dict(zip(range(11),np.arange(1400,2100,100)))
+#	PHI = dict(zip(range(6),np.linspace(1000,2000,6)))
+	PHI = dict(zip(range(16),np.linspace(1000,2000,16)))
 #		{
 #             '0':1,
 #             '1':2**3,
@@ -24,14 +23,17 @@ if __name__ == '__main__':
 	keyss= list(PHI.keys())
 	keyss.sort()
 	for iphi in keyss:
+			temp = int(PHI[iphi])
 			#---	
 			#---	densities
 				inums = lnums[ 0 ] - 1
 #				string[ inums ] = "\t1:\'NiNatom16KTemp%sK\',\n"%(int(PHI[iphi])) #--- change job name
-				string[ inums ] = "\t3:\'CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[iphi])) #--- change job name
+#				string[ inums ] = "\t3:\'CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[iphi])) #--- change job name
+				string[ inums ] = "\t0:\'NiMultTemp/Temp600K\',\n"%(int(PHI[iphi])) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
-				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(PHI[iphi])
+#				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(PHI[iphi])
+				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minim    ized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
 				#---
 
 				sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
