d59196cc0e9926116f242ed8dfc3869d89206d35
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-10 15:38:49 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 
-	nruns	 = np.arange(0,8)
+	nruns	 = [0] #np.arange(0,8)
 	#
-	nThreads = 8
+	nThreads = 16 #8
 	nNode	 = 1
 	#
 	jobname  = {
@@ -51,7 +51,8 @@ if __name__ == '__main__':
 					4:['data_minimized.txt'],
 					5:['data_init.txt','ScriptGroup.0.txt'], #--- only one partition! for multiple ones, use 'submit.py'
 					6:['FeNi_2000.dat'], 
-				 }[0] #--- to be copied from the above directory. set it to '0' if no file
+                    7:['sortieproc.0'], 
+				 }[7] #--- to be copied from the above directory. set it to '0' if no file
 	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
 	#
@@ -91,7 +92,7 @@ if __name__ == '__main__':
 				0:' -var natoms 100000 -var cutoff 3.52 -var ParseData 0 -var ntype 3 -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
 				6:' -var T 300 -var DataFile Equilibrated_300.dat',
 				4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
-				5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 16000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
+				5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 1000 -var ntype 5 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
 				51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
 				7:' -var buff 0.0 -var T 600.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_600.dat',
 				71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
@@ -122,7 +123,7 @@ if __name__ == '__main__':
 				7:[5,'p4','p3',1.0], #--- minimize, add vacancy, kart input, invoke kart
 				9:[5,'p4',51,'p3',1.0], #--- minimize, add vacancy, kart input, invoke kart
 				91:[5,'p3',1.0], #--- minimize, kart input, invoke kart
-                92:[5,'p7','p3',1.0], #--- create Topo_ignore, kart input ,invoke kart
+                92:[5,'p4','p7','p3',1.0], #--- minimize, add vacancy, create Topo_ignore, kart input ,invoke kart
 			  }[91]
 	Pipeline = list(map(lambda x:LmpScript[x],indices))
 #	Variables = list(map(lambda x:Variable[x], indices))
@@ -156,7 +157,7 @@ if __name__ == '__main__':
 			os.system( 'ln -s %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		if sourceFiles: 
 			for sf in sourceFiles:
-				os.system( 'ln -s %s/Run%s/%s %s' %(sourcePath, irun, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+				os.system( 'cp %s/Run%s/%s %s' %(sourcePath, irun, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		#---
 		makeOAR( path, 1, nThreads, durtn) # --- make oar script
 		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s' % ( writPath) ) # --- create folder & mv oar scrip & cp executable
