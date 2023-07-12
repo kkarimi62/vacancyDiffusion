68b3323ab192dbb52b539341b94b430949a52431
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-12 11:48:47 +0200

    update

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -3,14 +3,12 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-#	lnums = [ 33, 96   ]
-	lnums = [ 36, 96   ]
-#	lnums = [ 37, 103   ]
+	lnums = [ 37, 105   ]
 	string=open('simulations-ncbj.py').readlines() #--- python script
 #	string=open('simulations.py').readlines() #--- python script
 	#---
-#	PHI = dict(zip(range(6),np.linspace(1000,2000,6)))
-	PHI = dict(zip(range(16),np.linspace(1500,2500,16)))
+	PHI = dict(zip(range(6),np.linspace(1000,2000,6)))
+#	PHI = dict(zip(range(16),np.linspace(1500,2500,16)))
 #		{
 #             '0':1,
 #             '1':2**3,
@@ -24,18 +22,15 @@ if __name__ == '__main__':
 	keyss= list(PHI.keys())
 	keyss.sort()
 	for iphi in keyss:
-				temp = int(PHI[iphi])
+				temp = PHI[iphi]
 			#---	
 			#---	densities
 				inums = lnums[ 0 ] - 1
-#				string[ inums ] = "\t1:\'NiNatom16KTemp%sK\',\n"%(int(PHI[iphi])) #--- change job name
-#				string[ inums ] = "\t3:\'CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[iphi])) #--- change job name
-#				string[ inums ] = "\t0:\'NiMultTemp/Temp%sK\',\n"%(int(PHI[iphi])) #--- change job name
-				string[ inums ] = "\t3:\'Ni2ndMultTemp/Temp%sK\',\n"%(int(PHI[iphi])) #--- change job name
+				string[ inums ] = "\t3:\'cantorNatom1KmultiTemp/temp%s\',\n"%(iphi) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
-#				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(PHI[iphi])
-				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
+				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(temp)
+#				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
 				#---
 
 				sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
