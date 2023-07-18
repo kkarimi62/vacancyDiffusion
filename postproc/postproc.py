8ffdc05630aec9036945b8080201d9e8b3d760ba
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-18 11:48:47 +0200

    update

--- postproc/postproc.py
+++ postproc/postproc.py
@@ -1,63 +1,70 @@
 from backports import configparser
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
-	#--- parse conf. file
-	confParser = configparser.ConfigParser()
-	confParser.read('configuration.ini')
-	#--- set parameters
+    #--- parse conf. file
+    confParser = configparser.ConfigParser()
+    confParser.read('configuration.ini')
+    #--- set parameters
 #	confParser.set('parameters','itime0','5')
-	confParser.set('input files','lib_path',os.getcwd()+'/../../HeaDef/postprocess')
-	confParser.set('input files','input_path',argv)
-	#--- write
-	confParser.write(open('configuration.ini','w'))	
-	#--- set environment variables
+    confParser.set('input files','lib_path',os.getcwd()+'/../../HeaDef/postprocess')
+    confParser.set('input files','input_path',argv)
+    #--- write
+    confParser.write(open('configuration.ini','w'))	
+    #--- set environment variables
 
-	someFile = open( 'oarScript.sh', 'w' )
-	print('#!/bin/bash\n',file=someFile)
-	print('EXEC_DIR=%s\n'%( EXEC_DIR ),file=someFile)
+    someFile = open( 'oarScript.sh', 'w' )
+    print('#!/bin/bash\n',file=someFile)
+    print('EXEC_DIR=%s\n'%( EXEC_DIR ),file=someFile)
 #	print >> someFile, 'papermill --prepare-only %s/%s ./output.ipynb %s %s'%(EXEC_DIR,PYFIL,argv,argv2nd) #--- write notebook with a list of passed params
-	print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
-	someFile.close()										  
+    if convert_to_py:
+        print('ipython3 py_script.py\n',file=someFile)
+    else:
+        print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
+    someFile.close()										  
 #
 if __name__ == '__main__':
-	import os
+    import os
 #
-	runs	 = range(8)
-	jobname  = {
-				'3':'NiNatom16KTemp1400K', 
-				'4':'CantorNatom16KTemp1000KEnsemble8', 
-				}['4']
-	DeleteExistingFolder = True
-	readPath = os.getcwd() + {
-								'3':'/../simulations/NiNatom16KTemp1400K',
-								'4':'/../simulations/CantorNatom16KTemp1000KEnsemble8',
- 							}['4'] #--- source
-	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '23:59:59'
-	mem = '256gb'
-	partition = ['parallel','cpu2019','bigmem','single'][2] 
-	argv = "%s"%(readPath) #--- don't change! 
-	PYFILdic = { 
-		0:'postproc.ipynb',
-		}
-	keyno = 0
+    runs	 = range(8)
+    jobname  = {
+                '3':'NiNatom16KTemp1400K', 
+                '4':'CantorNatom16KTemp1000KEnsemble8', 
+                }['4']
+    DeleteExistingFolder = True
+    readPath = os.getcwd() + {
+                                '3':'/../simulations/NiNatom16KTemp1400K',
+                                '4':'/../simulations/CantorNatom16KTemp1000KEnsemble8',
+                            }['4'] #--- source
+    EXEC_DIR = '.'     #--- path for executable file
+    durtn = '23:59:59'
+    mem = '256gb'
+    partition = ['parallel','cpu2019','bigmem','single'][2] 
+    argv = "%s"%(readPath) #--- don't change! 
+    PYFILdic = { 
+        0:'postproc.ipynb',
+        }
+    keyno = 0
+    convert_to_py = True
 #---
 #---
-	PYFIL = PYFILdic[ keyno ] 
-	#--- update argV
-	#---
-	if DeleteExistingFolder:
-		os.system( 'rm -rf %s' % jobname ) # --- rm existing
-	# --- loop for submitting multiple jobs
-	for counter in runs:
-		print(' i = %s' % counter)
-		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
-		os.system( 'cp configuration.ini %s' % ( writPath ) ) #--- cp python module
-		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter) # --- make oar script
-		os.system( 'chmod +x oarScript.sh; cp oarScript.sh configuration.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
-		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
-						    --chdir %s -c %s -n %s %s/oarScript.sh'\
-						   % ( partition, mem, durtn, jobname[:4], counter, jobname[:4], counter, jobname[:4], counter \
-						       , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
-											 
+    PYFIL = PYFILdic[ keyno ] 
+    #--- update argV
+    if convert_to_py:
+        os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
+        PYFIL = 'py_script.py'
+    #---
+    if DeleteExistingFolder:
+        os.system( 'rm -rf %s' % jobname ) # --- rm existing
+    # --- loop for submitting multiple jobs
+    for counter in runs:
+        print(' i = %s' % counter)
+        writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
+        os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
+        os.system( 'cp configuration.ini %s' % ( writPath ) ) #--- cp python module
+        makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter) # --- make oar script
+        os.system( 'chmod +x oarScript.sh; cp oarScript.sh configuration.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
+        os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
+                            --chdir %s -c %s -n %s %s/oarScript.sh'\
+                           % ( partition, mem, durtn, jobname[:4], counter, jobname[:4], counter, jobname[:4], counter \
+                               , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
+
 
