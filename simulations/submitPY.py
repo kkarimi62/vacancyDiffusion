13ad8c25d3c7dd441fb9c6c1a23b1c054bf75667
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-25 08:51:01 +0200

    update

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -30,8 +30,7 @@ if __name__ == '__main__':
                 string[ inums ] = "\t4:\'nicocrNatom1K/md/temp%s\',\n"%(iphi) #--- change job name
             #---
                 inums = lnums[ 1 ] - 1
-                string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var rnd %%s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat\'%%np.random.randint(1001,9999),\n
-"%(temp)
+                string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var rnd %%s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat\'%%np.random.randint(1001,9999),\n"%(temp)
 #                 string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf"+" %"+"s"+" %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%(temp)
 #				string[ inums ] = "\t7:\' -var buff 0.0 -var T %s -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
                 #---
