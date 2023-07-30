f2bce32088adc4d4251d570a82813cf1753647bc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-30 11:00:32 +0200

    update

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
                     1:'NiNatom16KTemp1400K', 
                     2:'NiCoCrNatom1KTemp1400K', 
                     3:'Ni2ndMultTemp/Temp600K',#'NicoCrMultTemp/Temp600K',#'CantorNatom128KTemp1400K', 
-                    4:'nicocrNatom10KTemp1000K/temp0', 
+                    4:'nicocrNatom10KmultiTemp/temp0', 
                    }[4]
         sourcePath = os.getcwd() +\
                     {	
