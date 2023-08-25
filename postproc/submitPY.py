898c2880825dad4cfc5b02d9c2766a2fa02a5211
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-25 13:12:24 +0200

    update

--- postproc/submitPY.py
+++ postproc/submitPY.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 #    lnums = [ 30, 34 ]
     lnums = [ 31, 36 ]
     string=open('postproc_ncbj_slurm.py').readlines() #--- python script
-    string=open('postproc.py').readlines() #--- python script
+#    string=open('postproc.py').readlines() #--- python script
     #---
     PHI  = dict(zip(range(6),np.linspace(1000,2000,6)))
 #	PHI  = dict(zip(range(11),np.arange(1000,1440,40)))
