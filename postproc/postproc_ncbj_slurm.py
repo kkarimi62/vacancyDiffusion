a3e7db59b4728ef8c945de0ec8e40cc8ff32cfd7
kkarimi62 <kamran.karimi1362@gmail.com>
2023-11-02 11:24:48 +0100

    updates

--- postproc/postproc_ncbj_slurm.py
+++ postproc/postproc_ncbj_slurm.py
@@ -31,14 +31,14 @@ if __name__ == '__main__':
                 '5':'energyTimeseries/nicocr/kmc/NiCoCrNatom1KTemp1000K',
                 '3':'sro/nicocr/kmc/NiCoCrNatom1KTemp1000K',
                 '6':'sro/cantor/kmc/cantorNatom1KTemp1000K',
-                }['3']
+                }['6']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '3':'/../simulations/NiCoCrNatom1KTemp1000K',
                                 '4':'/../simulations/nicocrTemp1000K/n0',
                                 '5':'/../simulations/nicocr/kmc/NiCoCrNatom1KTemp1000K',
                                 '6':'/../simulations/cantorNatom1K/multipleTemp/temp0', 
-                            }['5'] #--- source
+                            }['6'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
     mem = '32gb'
