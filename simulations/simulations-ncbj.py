965be6969ef71e6a2af0c8e7d9cd83d0188098fc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-12-08 12:54:57 +0100

    update

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
         durtn = ['00:59:59','95:59:59','167:59:59','335:59:50','671:59:59'][ -1 ]
         qos='interq short normal long'.split()[3]
         mem = '16gb' #'22gb'
-        partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][1]
+        partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][2]
         #--
         DeleteExistingFolder = False
 
