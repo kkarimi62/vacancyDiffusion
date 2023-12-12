da13d3a62b2791f85fa8f119861b9f4e4c61dd79
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-12-12 12:27:46 +0100

    update

--- postproc/postproc_ncbj_slurm.py
+++ postproc/postproc_ncbj_slurm.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
                 '3':'msd/cantor/kmc/cantorNatom1KTemp1000K',
                 '7':'msdPerType/nicocr/kmc/NiCoCrNatom1KTemp1000K',
                 '8':'energyBarrierPerType/nicocr/kmc/NiCoCrNatom1KTemp1000K',
-                '9':'nicocr/md/nicocrNatom1K/md/temp5/short',
+                '9':'nicocr/md/nicocrNatom1K/md/temp0',
                 }['9']
     DeleteExistingFolder = False
     readPath = os.getcwd() + {
@@ -41,7 +41,7 @@ if __name__ == '__main__':
                                 '4':'/../simulations/nicocrTemp1000K/n0',
                                 '6':'/../simulations/cantorNatom1K/multipleTemp/temp0', 
                                 '5':'/../simulations/nicocr/kmc/NiCoCrNatom1KTemp1000K',
-                                '9':'/../simulations/nicocr/md/nicocrNatom1K/md/temp5',
+                                '9':'/../simulations/nicocr/md/nicocrNatom1K/md/temp0',
                             }['9'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
