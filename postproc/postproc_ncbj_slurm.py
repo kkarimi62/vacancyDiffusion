a1cdd67025a56532f544df321f5801402ad2b692
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-01-03 19:05:55 +0100

    Wigner-Seitz analysis

--- postproc/postproc_ncbj_slurm.py
+++ postproc/postproc_ncbj_slurm.py
@@ -24,7 +24,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv, argvv):
 if __name__ == '__main__':
     import os
 
-    runs     = range(8)
+    runs     = range(1)
     nNode    = 1
     nThreads = 1
     jobname  = {
@@ -39,8 +39,8 @@ if __name__ == '__main__':
                 '12':'msd_definition/ni/temp0_6th',
                 '13':'ni/koreanPotential/size0',
                 '14':'ni/shengPotential/temp0_vac_ws',
-                '15':'vacancy/shengPotential/temp0_4th',
-                }['14']
+                '15':'vacancy/shengPotential/temp0_vac',
+                }['15']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '3':'/../simulations/NiCoCrNatom1KTemp1000K',
@@ -52,19 +52,19 @@ if __name__ == '__main__':
                                 '12':'/../simulations/ni/koreanPotential/NiNatom1KTemp1000K', 
                                 '13':'/../simulations/ni/koreanPotential/size0',
                                 '14':'/../simulations/ni/shengPotential/temp0',
-                                '15':'/ni/shengPotential/temp0_4th',
-                            }['14'] #--- source
+                                '15':'/ni/shengPotential/temp0_vac',
+                            }['15'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
     mem = '32gb'
-    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][2]
+    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][-1]
     argv = "%s"%(readPath) #--- don't change! 
     PYFILdic = { 
         0:'postproc.ipynb',
         1:'vacancyDynamics.ipynb',
         2:'test.ipynb',
         }
-    keyno = 0
+    keyno = 1
     convert_to_py = True
 #---
 #---
