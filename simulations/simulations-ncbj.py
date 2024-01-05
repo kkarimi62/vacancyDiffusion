7b08d986e11f24410f76f56195aef933dbece899
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-01-05 15:26:08 +0100

    update

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
                     4:'nicocrTemp1000K/n1',
                     6:'ni/koreanPotential/size0',
                     7:'ni/FoilesPotential/temp0',
-                    5:'cantorNatom1K/multipleTemp/temp0',
+                    5:'cantorNatom1K/multipleTemp/temp4',
                    }[5]
         sourcePath = os.getcwd() +\
                     {	
@@ -104,7 +104,7 @@ if __name__ == '__main__':
                     'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
                     'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
                     'p2':' %s 3.52 135.0 67.0 135.0 data.txt 5'%(os.getcwd()+'/../postprocess'),
-                    'p3':' data_minimized.dat init_xyz.conf %s 1000.0'%(os.getcwd()+'/lmpScripts'),
+                    'p3':' data_minimized.dat init_xyz.conf %s 1800.0'%(os.getcwd()+'/lmpScripts'),
                     'p4':' data_perfect_crystal.dat data_defect_crystal.dat %s 1'%(os.getcwd()+'/lmpScripts'),
                     'p5':' ',
                      1.0:'DataFile=data_minimized.dat',
