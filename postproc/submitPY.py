7abf68a21541ccbae4b7cb2ee92257758213d5cc
kkarimi62 <kamran.karimi1362@gmail.com>
2024-01-08 10:38:05 +0100

    updates

--- postproc/submitPY.py
+++ postproc/submitPY.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
                 temp = PHI[key]
             #---	
                 inums = lnums[ 0 ] - 1
-                string[ inums ] = "\t\'5\':\'nicocr/kmc/NiCoCrNatom1KTemp%sK/msd\',\n" % (temp) #--- change job name
+                string[ inums ] = "\t\'5\':\'cantorNatom1K/multipleTemp/temp%s/msd\',\n" % (key) #--- change job name
         #---	densities
                 inums = lnums[ 1 ] - 1
-                string[ inums ] = "\t\'5\':\'/nicocr/kmc/NiCoCrNatom1KTemp%sK\',\n"%(temp)
+                string[ inums ] = "\t\'5\':\'/cantorNatom1K/multipleTemp/temp%s\',\n"%(key)
         #
                 sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
                 os.system( 'python3 junk%s.py'%count )
